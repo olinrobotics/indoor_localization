cee18da715c0abaf31641b7dce865a557093e76c
Kubo <olingravl@gmail.com>
2019-05-15 22:55:42 -0400

    Attempt to fix coordinate frame of apriltags

--- src/publish_tf_as_odom.cpp
+++ src/publish_tf_as_odom.cpp
@@ -1,6 +1,7 @@
 #include <nav_msgs/Odometry.h>
 #include <ros/ros.h>
 #include <tf2_ros/transform_listener.h>
+#include <tf/transform_datatypes.h>
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "tf_to_odom");
@@ -28,10 +29,23 @@ int main(int argc, char **argv) {
     odom.pose.pose.position.x = transformStamped.transform.translation.x;
     odom.pose.pose.position.y = transformStamped.transform.translation.y;
     odom.pose.pose.position.z = transformStamped.transform.translation.z;
-    odom.pose.pose.orientation.w = transformStamped.transform.rotation.w;
-    odom.pose.pose.orientation.x = transformStamped.transform.rotation.x;
-    odom.pose.pose.orientation.y = transformStamped.transform.rotation.y;
-    odom.pose.pose.orientation.z = transformStamped.transform.rotation.z;
+
+    tf::Quaternion q1(transformStamped.transform.rotation.x,
+                      transformStamped.transform.rotation.y,
+                      transformStamped.transform.rotation.z,
+                      transformStamped.transform.rotation.w);
+    tf::Matrix3x3 m(q1);
+    double roll, pitch, yaw;
+    m.getRPY(roll, pitch, yaw);
+
+    tf2::Quaternion q2;
+    q2.setRPY(roll, pitch, -yaw);
+    q2.normalize();
+
+    odom.pose.pose.orientation.w = q2[3];
+    odom.pose.pose.orientation.x = q2[0];
+    odom.pose.pose.orientation.y = q2[1];
+    odom.pose.pose.orientation.z = q2[2];
     odom.header.frame_id = "map";
     odom.child_frame_id = "tag_cam";
 
